459db240be4760999355f9a6907be82f1eed5426
manudeep <manudeep1000@gmail.com>
2026-06-26 15:17:53 +0530

    46th

---
+++ checkSubstring.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class checkSubstring {
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+String str=sc.nextLine();
+String sub=sc.nextLine();
+if(str.contains(sub))
+System.out.println("Substring Present");
+else
+System.out.println("Substring Not Present");
+}
+}
